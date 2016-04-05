ba148460b910f320593a87f2f8dd1aaf6083da10
Henri Normak <henri.normak@gmail.com>
2016-04-05 10:20:17 +0300

    Install dependencies
    This is optional to the extent that if the user de-selects both options, then nothing is installed. However, by default, both lambda-foundation and lambda-tools are installed.

--- generators/app/index.js
+++ generators/app/index.js
@@ -9,72 +9,143 @@ const mkdirp = require('mkdirp');
  *  stubs for 'api.json' and 'cf.json' files.
  */
 module.exports = generators.Base.extend({
-    prompting: function() {
-        const done = this.async();
-
-        const prompts = [
-            {
-                type: 'input',
-                name: 'serviceName',
-                message: 'Service name',
-                store: true,
-                default: this.appname.trim().replace(/\s+/g, '-')
-            },
-            {
-                type: 'input',
-                name: 'serviceDescription',
-                message: 'Service description'
-            },
-            {
-                type: 'input',
-                name: 'serviceLicense',
-                message: 'License (API)',
-                store: true,
-                default: 'ISC',
-                validate: function(value) {
-                    // Simple rule, make sure the value is present
-                    return value.length > 0 ? true : 'License can\'t be empty';
+    prompting: {
+        service: function() {
+            // Ask for the basic information about this service
+            // Notice, this info is also stored for future reference
+            const prompts = [
+                {
+                    type: 'input',
+                    name: 'serviceName',
+                    message: 'Service name',
+                    default: function() {
+                        return this.appname.trim().replace(/\s+/g, '-');
+                    }.bind(this),
+                    filter: function(value) {
+                        return value.toLowerCase().replace(/\s+/g, '-');
+                    }
+                },
+                {
+                    type: 'input',
+                    name: 'serviceDescription',
+                    message: 'Service description'
+                },
+                {
+                    type: 'input',
+                    name: 'serviceLicense',
+                    message: 'License (API)',
+                    default: 'ISC',
+                    store: true,
+                    validate: function(value) {
+                        // Simple rule, make sure the value is present
+                        return value.length > 0 ? true : 'License can\'t be empty';
+                    }
                 }
-            },
-            {
-                type: 'input',
-                name: 'authorEmail',
-                message: 'Author (email)',
-                store: true,
-                validate: function(value) {
-                    // Simple rule, make sure the value is present
-                    return value.length > 0 ? true : 'Email can\'t be empty';
+            ];
+
+            const done = this.async();
+            this.prompt(prompts, function(answers) {
+                this.service = {
+                    name: answers.serviceName,
+                    description: answers.serviceDescription,
+                    license: answers.serviceLicense
+                };
+
+                done();
+            }.bind(this));
+        },
+
+        author: function() {
+            // Ask about the author of this service
+            const prompts = [
+                {
+                    type: 'input',
+                    name: 'authorEmail',
+                    message: 'Author email',
+                    store: true,
+                    validate: function(value) {
+                        // Simple rule, make sure the value is present
+                        return value.length > 0 ? true : 'Email can\'t be empty';
+                    }
+                },
+                {
+                    type: 'input',
+                    name: 'authorName',
+                    message: 'Author name',
+                    store: true,
+                    validate: function(value) {
+                        // Simple rule, make sure the value is present
+                        return value.length > 0 ? true : 'Name can\'t be empty';
+                    }
                 }
-            },
-            {
-                type: 'input',
-                name: 'authorName',
-                message: 'Author (name)',
-                store: true,
-                validate: function(value) {
-                    // Simple rule, make sure the value is present
-                    return value.length > 0 ? true : 'Name can\'t be empty';
+            ];
+
+            const done = this.async();
+            this.prompt(prompts, function(answers) {
+                this.author = {
+                    name: answers.authorName,
+                    email: answers.authorEmail
+                };
+
+                done();
+            }.bind(this));
+        },
+
+        dependencies: function() {
+            // Ask about installing dependencies
+            const dependencies = [
+                {
+                    // LT is always installed as dev-dependency
+                    name: 'lambda-tools',
+                    value: {
+                        name: '@testlio/lambda-tools',
+                        dev: true
+                    },
+                    checked: true
+                },
+                {
+                    name: 'lambda-foundation',
+                    value: {
+                        name: '@testlio/lambda-foundation',
+                        dev: false
+                    },
+                    checked: true
                 }
-            }
-        ];
+            ];
 
-        this.prompt(prompts, function (answers) {
-            this.service = {
-                name: answers.serviceName.toLowerCase().replace(/\s+/g, '-'),
-                description: answers.serviceDescription,
-                license: answers.serviceLicense
-            };
+            const prompts = [
+                {
+                    type: 'checkbox',
+                    name: 'dependencies',
+                    message: 'Install dependencies',
+                    choices: dependencies
+                }
+            ];
 
-            this.author = {
-                name: answers.authorName,
-                email: answers.authorEmail
-            };
+            const done = this.async();
+            this.prompt(prompts, function(answers) {
+                this.dependencies = [];
 
-            done();
-        }.bind(this));
+                if (answers.dependencies) {
+                    this.dependencies = this.dependencies.concat(answers.dependencies);
+                }
+
+                done();
+            }.bind(this));
+        }
     },
 
     writing: function() {
+        // Install appropriate dependencies
+        if (this.dependencies) {
+            this.dependencies.forEach(function(dependency) {
+                this.npmInstall([dependency.name], {
+                    saveDev: dependency.dev,
+                    save: !dependency.dev
+                });
+            }.bind(this));
+        }
+
         const template = {
             service: this.service,
             author: this.author
